549b5abe2e023df694f7999e55a6a8a987330c96
Dmitry Berezovsky <dmitri.berezovsky@logicify.com>
2014-04-24 01:07:18 +0300

    Volume icon & google chrome added to startup scenario

--- awesome/autostart.lua
+++ awesome/autostart.lua
@@ -9,13 +9,16 @@ and services which should be started immediately after initialization of desktop
 local helpers = require("helpers")
 
 -- Start screen sever daemon
-helpers.run_once("xscreensaver", "-no-splash")
+helpers.run_once("gnome-screensaver")
 
 -- Setup keyboard layout
 helpers.run_once("setxkbmap", "-option grp:caps_toggle \"us,ru\"")
 
 -- Network Manager
 helpers.run_once("nm-applet")
+
+-- Volume control icon
+helpers.run_once("volumeicon")
 
 -- Power manager
 helpers.run_once("xfce4-power-manager")
@@ -25,3 +28,9 @@ helpers.run_once("synapse", "-s")
 
 -- Enable numlock
 helpers.run_once("numlockx", "on")
+
+-- Enable numlock
+helpers.run_once("volumeicon")
+
+-- Google chrome
+helpers.run_once("/opt/google/chrome/google-chrome --no-startup-window")
